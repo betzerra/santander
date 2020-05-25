36020c3a37fcb814bc2acf94cb67e9f991d4ab81
Ezequiel Becerra <ezequiel.becerra@gmail.com>
2020-05-25 14:35:47 -0300

    UI tweaks

--- app/helpers/unit_trusts_helper.rb
+++ app/helpers/unit_trusts_helper.rb
@@ -1,2 +1,14 @@
 module UnitTrustsHelper
+  def self.deposit_time_badge(time)
+    case time
+    when 0
+      '<span class="badge badge-soft-success">Inmediato</span>'
+    when 1..24
+      "<span class=\"badge badge-soft-info\">#{time} horas</span>"
+    when 25..48
+      "<span class=\"badge badge-soft-warning\">#{time} horas</span>"
+    else
+      "<span class=\"badge badge-soft-danger\">#{time} horas</span>"
+    end
+  end
 end
